8b18ce4b15f80871bcb00726b4385dc1a512c07a
Serhat Sarı <serhat.sari@nesine.com>
2018-06-26 11:20:10 +0300

    Initial Commit

---
+++ MyGalaxyOfAppleSdks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyGalaxyOfAppleSdks
+//
+//  Created by Serhat Sarı on 26.06.2018.
+//  Copyright © 2018 Serabisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
